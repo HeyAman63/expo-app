c2f3c845a232f6ff9eea012f4ddc64d7003ce43b
Aman Chaurasiya <chaurasiyaaman741@gmail.com>
2025-12-25 21:41:04 +0530

    auth implemented

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,38 @@
+import {Inngest} from 'inngest'
+import {ConnectDb} from './db.js'
+import {User} from '../models/user.model.js'
+
+export const inngest =new Inngest({id:"ecommerce-app"});
+
+const syncUser = inngest.createFunction(
+    {id:"sync-user"},
+    {event:"clerk/user.created"},
+    async({event})=>{
+        await ConnectDb();
+        const {id,email_addresses,first_name,last_name,image_url} = event.data;
+
+        const newUser = {
+            clerkId:id,
+            email:email_addresses[0]?.email_address,
+            name:`${first_name || ""} ${last_name || ""}` || "User",
+            imageUrl: image_url,
+            addresses:[],
+            wishList:[],
+        }
+
+        await User.create(newUser);
+    }
+)
+
+const deleteUserFromDb = inngest.createFunction(
+    {id:"delete-user-from-db"},
+    {event:"clerk/user.deleted"},
+    async({event})=>{
+        await ConnectDb();
+        const {id} = event.data;
+
+        await User.deleteOne({clerkId:id})
+    }
+)
+
+export const functions = [syncUser,deleteUserFromDb]
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,16 @@ import { ENV } from './config/env.js'
 import path from 'path';
 import { ConnectDb } from './config/db.js';
 import {clerkMiddleware} from '@clerk/express'
+import {serve} from 'inngest/express'
+
+import { functions,inngest } from './config/inngest.js';
 
 
-const app = express()
+const app = express();
+app.use(express.json());
 app.use(clerkMiddleware()); // req.auth
+
+app.use('/api/inngest',serve({client:inngest, functions}));
 
 // const __dirname = path.resolve();
 
@@ -23,8 +29,11 @@ app.get('/api/health',(req,res)=>{
 //         res.sendFile(path.join(__dirname,"../admin", "dist","index.html"));
 //     })
 // }
+const startServer = async()=>{
+    await ConnectDb();
+    app.listen(ENV.PORT,()=>{
+        console.log("Server is up and runnning on port 4000");
+    })
+}
 
-app.listen(ENV.PORT,()=>{
-    console.log("Server is up and runnning on port 4000");
-    ConnectDb();
-})
+startServer();
